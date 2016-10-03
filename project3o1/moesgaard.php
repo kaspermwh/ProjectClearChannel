da96f18d91a23a3f01d5abad8338e8841e3b34e3
melage <age-melage@hotmail.com>
2016-10-03 09:38:39 +0200

    Add files via upload

---
+++ project3o1/moesgaard.php
@@ -0,0 +1,27 @@
+<?php
+    include("htmlstart.php");
+    start("Culture Explorer | Botanisk Have", "Welcome to Terapi Hobby");
+?>
+        <div class="moesgaard">
+            <article>
+                <h2><span class="blackbg">Moesgaard Museum</span></h2>
+                <h3><span class="blackbg">Experience human diversity in a great setting. Exciting and ethnographic exhibits for kids and adults.</span></h3>
+                <p><span class="blackbg">See More...</span></p>
+            </article>   
+        </div>
+        <div class="transport_means">
+            <article>
+                <h2>Method of transportation</h2>
+            </article>
+            <nav class="transport_choose">
+                <ul>
+                    <li class="group"><a href="#"><img src="img/buschoose.png" alt="See bus routes!"></a></li>
+                    <li class="group"><a href="#"><img src="img/carchoose.png" alt="See bus routes!"></a></li>
+                    <li class="group"><a href="#"><img src="img/walkchoose.png" alt="See bus routes!"></a></li>
+                    <li class="group"><a href="#"><img src="img/bikechoose.png" alt="See bus routes!"></a></li>
+                </ul>
+            </nav>
+        </div>
+<?php
+    include ("htmlend.php");
+?>
